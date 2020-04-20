3444c593a412525af74ef5c96855260a7d300caf
Jesus Aranda <aranda.jm@icloud.com>
2020-04-20 20:58:10 -0300

    Binding form inputs with UserModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -12,13 +12,10 @@ import { RegisterComponent } from './pages/register/register.component';
     AppComponent,
     HomeComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from 'src/app/models/usuario.model';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
 })
 export class RegisterComponent implements OnInit {
+  usuario: UsuarioModel;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.usuario = new UsuarioModel();
+    this.usuario.email = 'jesus@gmail.com';
+  }
+
+  registrarUsuario() {
+    console.log(this.usuario);
+  }
 }
