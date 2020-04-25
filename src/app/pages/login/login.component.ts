3183c39e56a58c127ee0aefa7b3612744b7b469d
Jesus Aranda <aranda.jm@icloud.com>
2020-04-25 19:34:54 -0300

    Adding login form and showing validation error messages

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioModel } from '../../models/usuario.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
 })
 export class LoginComponent implements OnInit {
+  usuario: UsuarioModel;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.usuario = new UsuarioModel();
+  }
+
+  login(loginForm: NgForm) {
+    if (loginForm.invalid) {
+      console.log(loginForm);
+      return;
+    }
+    console.log(loginForm);
+    console.log(this.usuario);
+  }
 }
