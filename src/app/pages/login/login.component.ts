d699b321d324e2221c4ab5ababb1652082f41e09
Jesus Aranda <aranda.jm@icloud.com>
2020-04-26 00:23:07 -0300

    Creating the functionality to login into the application using Firebase Rest api

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioModel } from '../../models/usuario.model';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +9,7 @@ import { NgForm } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
   usuario: UsuarioModel;
-  constructor() {}
+  constructor(private _authService: AuthService) {}
 
   ngOnInit(): void {
     this.usuario = new UsuarioModel();
@@ -19,7 +20,13 @@ export class LoginComponent implements OnInit {
       console.log(loginForm);
       return;
     }
-    console.log(loginForm);
-    console.log(this.usuario);
+    this._authService.login(this.usuario).subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (err) => {
+        console.log(err.error.error.message);
+      }
+    );
   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -6,15 +6,19 @@ import { UsuarioModel } from '../models/usuario.model';
   providedIn: 'root',
 })
 export class AuthService {
-  // Url to create new users
-  // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
-
-  // https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=[API_KEY]
   private url: string = 'https://identitytoolkit.googleapis.com/v1/';
   private apiKey: string = 'AIzaSyDQYydhCWkaXDG6Q_UUvLO1EM4xus0smtU';
+
   constructor(private http: HttpClient) {}
 
-  login(user: UsuarioModel) {}
+  login(user: UsuarioModel) {
+    const payload = {
+      email: user.email,
+      password: user.password,
+      returnSecureToken: true,
+    };
+    return this.http.post(this.getUrl('accounts:signInWithPassword'), payload);
+  }
 
   register(user: UsuarioModel) {
     const payload = {
